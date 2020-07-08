07ff07ee9bb7984c9c997801726dd8c61d3a1427
ianzures <i.anzures514@gmail.com>
2020-07-08 12:45:04 -0400

    Makes axios request to trending gifs and logs the first gif object.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
 import React from 'react';
 import SearchField from './components/SearchField.js';
 import GifList from './components/GifList';
-
+import axios from 'axios';
 
 class App extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            gifs: [
-                {
-                    id: 1,
-                    url: 'http://fakeimg.pl/300/'
-                },
-                {
-                    id: 2,
-                    url: 'http://fakeimg.pl/300/'
-                },
-                {
-                    id: 3,
-                    url: 'http://fakeimg.pl/300/'
-                }
-            ]
+            gifs: []
         }
+    }
+
+    searchGif = (term) => {
+        console.log(term);
+        const url = 'http://api.giphy.com/v1/gifs/trending?api_key=9S4WDGPk7Csoi0lZi4HPSHIwlejTxR2I';
+        axios.get(url).then(result => console.log(result.data.data[0]));
     }
 
     render() {
         return (
             <div className="App">
-                <SearchField />
-                <GifList gifs={this.state.gifs}/>
+                <SearchField searchGif={this.searchGif} />
+                <GifList gifs={this.state.gifs} />
             </div>
         );
     }
