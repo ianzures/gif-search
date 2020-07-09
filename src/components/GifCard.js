8b4952f41ba1716c453b38bf71e7a9dde6564d9e
ianzures <i.anzures514@gmail.com>
2020-07-08 23:00:49 -0400

    CSS commit

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -3,10 +3,8 @@ import React from 'react';
 const item = {
     boxShadow: '2px 2px 4px 0 #ccc',
     boxSizing: 'borderBox',
-    display: 'inlineBlock',
     margin: '0 0 1.5em',
     padding: '1em',
-    //width: '100 %',
 }
 
 const GifCard = (image) => {
